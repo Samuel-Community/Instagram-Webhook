5399506e96d6dc627b545218c316af0f81d6ec41
Samuel <37155363+TutoRapide@users.noreply.github.com>
2021-04-05 22:15:40 +0200

    All UP instagram

--- index.js
+++ index.js
@@ -1,48 +1,55 @@
-/*const Insta = require('scraper-instagram');
-const InstaClient = new Insta();
+const {get, post} = require("axios");
+const sesi = "sessionid"; //instagram.com
+const path = require('path');
+const {readFileSync, writeFileSync} = require('fs');
 
-InstaClient.authBySessionId('45331080708%3AekXzJ24PhAhIPz%3A28')
-	.then(account => console.log(account))
-	.catch(err => console.error(err));
+console.log('Instagram Webhook Start')
 
-  InstaClient.getProfile('tutorapide')
-	.then(profile => console.log(profile))
-	.catch(err => console.error(err));
+setInterval(function() {
 
+    let current = {}
+    try { current = JSON.parse(readFileSync(path.join(__dirname, 'current.json'), 'utf8')) } catch(e) {}
 
-*/
+    get(`https://instagram.com/TutoRapide/?__a=1`, { headers: { 
+        cookie: `sessionid=${sesi}` 
+    }
+}).then(async (res) => {
 
+     try {
 
-const {post, get} = require('axios');
+         let latest = await res.data.graphql.user.edge_owner_to_timeline_media.edges[0].node.shortcode
+  
+         if (latest === current) return
+  
+         current = latest
 
-get("https://api.instagram.com/v1/users/119911386621358/media/recent",{
-headers: {
-    "Content-Type": "application/json"
-}
-}).then((res) => {
-    console.log(res.data)
-});
+         console.log( latest === current)
 
+        await post('urlwebhook', {
+             "username": res.data.graphql.user.full_name,
+             "avatar_url": res.data.graphql.user.profile_pic_url_hd,
+             "embeds": [
+                 {
+                     title: `New post by ${res.data.graphql.user.full_name}`,
+                     color: `15258703`,
+                     url: `https://www.instagram.com/p/${res.data.graphql.user.edge_owner_to_timeline_media.edges[0].node.shortcode}`,
+                     description: res.data.graphql.user.edge_owner_to_timeline_media.edges[0].node.edge_media_to_caption.edges[0].node.text,
+                     author: {
+                         name: res.data.graphql.user.full_name,
+                         icon_url: res.data.graphql.user.profile_pic_url_hd
+                     },
+                     image: {
+                         url: res.data.graphql.user.edge_owner_to_timeline_media.edges[0].node.display_url
+                     }
+                 }
+             ],
+         });
+         
+     } catch (error) {
+         
+     }
+     writeFileSync(path.join(__dirname, 'current.json'), JSON.stringify(current), 'utf8')
+     console.log('ok actu')
+    });
 
-
-
-/*const fetch = require('node-fetch')
-fetch('https://www.instagram.com/tutorapide/?__a=1')
-
-.then(res => res.json())
-.then((json) => {
-  console.log(json);
-
-
-})*/
-
-/*fetch('https://www.instagram.com/tutorapide/?__a=1', {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({
-      username: json.graphql.user.full_name
-    }),
-
-}).then(res => res.json())
-  .then(json => console.log(json));*/
-
+}, 20000)
