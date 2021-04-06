23db7667d963ee5762fe123a50b6d38165c808f6
Samuel <37155363+TutoRapide@users.noreply.github.com>
2021-04-07 01:31:04 +0200

    syntaxe up

--- index.js
+++ index.js
@@ -1,51 +1,52 @@
-const {get, post} = require("axios");
+const { get, post } = require("axios");
 const path = require('path');
-const {readFileSync, writeFileSync} = require('fs');
-const {webhook, sessionID, user} = require("./config.js");
+const { readFileSync, writeFileSync } = require('fs');
+const { webhook, sessionID, user } = require("./config.js");
 
 console.log('Instagram Webhook Start')
 
-setInterval(function() {
+setInterval(function () {
 
     let current = {}
-    try { current = JSON.parse(readFileSync(path.join(__dirname, 'current.json'), 'utf8')) } catch(e) {}
+    try { current = JSON.parse(readFileSync(path.join(__dirname, 'current.json'), 'utf8')) } catch (e) { }
 
-    get(`https://instagram.com/${user}/?__a=1`, { headers: { 
-        cookie: `sessionid=${sessionID}` 
-    }
-}).then(async (res) => {
+    get(`https://instagram.com/${user}/?__a=1`, {
+        headers: {
+            cookie: `sessionid=${sessionID}`
+        }
+    }).then(async (res) => {
 
-     try {
+        try {
 
-         let latest = await res.data.graphql.user.edge_owner_to_timeline_media.edges[0].node.shortcode
-  
-         if (latest === current) return
-  
-         current = latest
+            let latest = await res.data.graphql.user.edge_owner_to_timeline_media.edges[0].node.shortcode
 
-         console.log( latest === current)
+            if (latest === current) return
 
-        await post(webhook, {
-             "username": res.data.graphql.user.full_name,
-             "avatar_url": res.data.graphql.user.profile_pic_url_hd,
-             "embeds": [
-                 {
-                     title: `New post by ${res.data.graphql.user.full_name}`,
-                     color: `15258703`,
-                     url: `https://www.instagram.com/p/${res.data.graphql.user.edge_owner_to_timeline_media.edges[0].node.shortcode}`,
-                     description: res.data.graphql.user.edge_owner_to_timeline_media.edges[0].node.edge_media_to_caption.edges[0].node.text,
-                     author: { name: res.data.graphql.user.full_name, icon_url: res.data.graphql.user.profile_pic_url_hd },
-                     image: { url: res.data.graphql.user.edge_owner_to_timeline_media.edges[0].node.display_url },
-                     footer: { icon_url: res.data.graphql.user.profile_pic_url_hd, text: res.data.graphql.user.full_name },
-                 }
-             ],
-         });
-         
-     } catch (error) {
-         
-     }
-     writeFileSync(path.join(__dirname, 'current.json'), JSON.stringify(current), 'utf8')
-     console.log('ok actu')
+            current = latest
+
+            console.log(latest === current)
+
+            await post(webhook, {
+                "username": res.data.graphql.user.full_name,
+                "avatar_url": res.data.graphql.user.profile_pic_url_hd,
+                "embeds": [
+                    {
+                        title: `New post by ${res.data.graphql.user.full_name}`,
+                        color: `15258703`,
+                        url: `https://www.instagram.com/p/${res.data.graphql.user.edge_owner_to_timeline_media.edges[0].node.shortcode}`,
+                        description: res.data.graphql.user.edge_owner_to_timeline_media.edges[0].node.edge_media_to_caption.edges[0].node.text,
+                        author: { name: res.data.graphql.user.full_name, icon_url: res.data.graphql.user.profile_pic_url_hd },
+                        image: { url: res.data.graphql.user.edge_owner_to_timeline_media.edges[0].node.display_url },
+                        footer: { icon_url: res.data.graphql.user.profile_pic_url_hd, text: res.data.graphql.user.full_name },
+                    }
+                ],
+            });
+
+        } catch (error) {
+
+        }
+        writeFileSync(path.join(__dirname, 'current.json'), JSON.stringify(current), 'utf8')
+        console.log('ok actu')
     });
 
 }, 20000)
