952151c560ade0cc817a5dc2709fba6616f797d1
Samuel <37155363+TutoRapide@users.noreply.github.com>
2021-04-07 11:44:04 +0200

    Update

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
-const { get, post } = require("axios");
-const path = require('path');
-const { readFileSync, writeFileSync } = require('fs');
-const { webhook, sessionID, user } = require("./config.js");
+const { get, post } = require("axios"),
+ path = require('path'),
 
-console.log('Instagram Webhook Start')
+ { readFileSync, writeFileSync } = require('fs'),
+
+ { webhook, sessionID, user } = require("./config.js");
+
+console.log('Instagram Webhook Start');
 
 setInterval(function () {
 
     let current = {}
-    try { current = JSON.parse(readFileSync(path.join(__dirname, 'current.json'), 'utf8')) } catch (e) { }
+    try { current = JSON.parse(readFileSync(path.join(__dirname, 'current.json'), 'utf8')) } catch (e) { };
 
     get(`https://instagram.com/${user}/?__a=1`, {
         headers: {
@@ -20,18 +22,16 @@ setInterval(function () {
 
             let latest = await res.data.graphql.user.edge_owner_to_timeline_media.edges[0].node.shortcode
 
-            if (latest === current) return
+            if (latest === current) return;
 
             current = latest
-
-            console.log(latest === current)
 
             await post(webhook, {
                 "username": res.data.graphql.user.full_name,
                 "avatar_url": res.data.graphql.user.profile_pic_url_hd,
                 "embeds": [
                     {
-                        title: `Nouveau poste de ${res.data.graphql.user.full_name}`,
+                        title: `New post by ${res.data.graphql.user.full_name}`,
                         color: `15258703`,
                         url: `https://www.instagram.com/p/${res.data.graphql.user.edge_owner_to_timeline_media.edges[0].node.shortcode}`,
 
@@ -40,7 +40,7 @@ setInterval(function () {
                         author: { name: res.data.graphql.user.full_name, icon_url: res.data.graphql.user.profile_pic_url_hd },
 
                         image: { url: res.data.graphql.user.edge_owner_to_timeline_media.edges[0].node.display_url },
-                        
+
                         footer: { icon_url: res.data.graphql.user.profile_pic_url_hd, text: res.data.graphql.user.full_name },
                     }
                 ],
@@ -50,7 +50,7 @@ setInterval(function () {
 
         }
         writeFileSync(path.join(__dirname, 'current.json'), JSON.stringify(current), 'utf8')
-        console.log('ok actu')
+        console.log('New POST')
     });
 
 }, 20000)
